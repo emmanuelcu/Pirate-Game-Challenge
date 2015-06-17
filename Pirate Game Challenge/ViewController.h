65b21f66de68612b12ffa0ebda03528690bbd7e4
Emmanuel <emmanuelcu@gmail.com>
2015-06-16 22:47:45 -0500

    Initial Commit

---
+++ Pirate Game Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pirate Game Challenge
+//
+//  Created by Emmanuel Cuevas on 6/16/15.
+//  Copyright (c) 2015 Emmanuel Cuevas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pirate Game Challenge/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Pirate Game Challenge
+//
+//  Created by Emmanuel Cuevas on 6/16/15.
+//  Copyright (c) 2015 Emmanuel Cuevas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
